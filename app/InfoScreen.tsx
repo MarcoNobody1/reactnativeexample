753743804e57db707009ff0b54b30695d368c3b1
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-27 20:56:46 +0100

    links to tels and maps finished

--- app/InfoScreen.tsx
+++ app/InfoScreen.tsx
@@ -1,11 +1,48 @@
-import { SafeAreaView, Text } from "react-native";
+import { Linking, SafeAreaView, Text, View } from "react-native";
 import { InfoProps } from "../interfaces/NavigationInterfaces";
 import { styles } from "../styles/styles";
 
 export const InfoScreen: React.FC<InfoProps> = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is info's screen</Text>
+      <Text style={styles.infoheader}>Hotel Info</Text>
+
+      <View style={styles.infotable}>
+        <Text
+          style={styles.infocell}
+          onPress={() => Linking.openURL("tel:+34910555555")}
+        >
+          Reception phone number: +34 910 555 555
+        </Text>
+        <Text
+          style={styles.infocell}
+          onPress={() => Linking.openURL("tel:+34910555556")}
+        >
+          ROOM Phone NUMBER: +34 910 555 556 #EXT (Room number)
+        </Text>
+        <Text
+          style={styles.infocell}
+          onPress={() => Linking.openURL("tel:+34912555555")}
+        >
+          24H HELPLINE: +34 912 555 555
+        </Text>
+        <Text
+          style={styles.infocell}
+          onPress={() =>
+            Linking.openURL(
+              "https://www.google.com/maps/search/?api=1&query=CALLE+DE+LA+PRINCESA,+31.+Madrid+28008"
+            )
+          }
+        >
+          Address: CALLE DE LA PRINCESA, 31. Madrid 28008
+        </Text>
+        <Text
+          style={styles.infocell}
+          onPress={() => Linking.openURL("tel:+112")}
+        >
+          EMERGENCY NUMBER: 112
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
