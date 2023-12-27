fd4db4e50951eba8b04b6cf318897c49dcbfad4f
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-27 20:00:28 +0100

    new screens and components added to main app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,59 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "./app/HomeScreen";
-import { InfoScreen } from "./app/InfoScreen";
+import { CheckInScreen } from "./app/CheckInScreen";
 import { RootStackParamList } from "./interfaces/NavigationInterfaces";
-
+import { Logo } from "./components/logo";
+import { Pressable, Text } from "react-native";
+import { styles } from "./styles/styles";
+import { InfoScreen } from "./app/InfoScreen";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={({ navigation }) => ({
+          headerTitle: (props) => <Logo navigation={navigation} />,
+          headerRight: () => (
+            <Pressable onPress={() => navigation.navigate("Info")}>
+              <Text style={styles.linkText}>Info</Text>
+            </Pressable>
+          ),
+          headerStyle: {
+            backgroundColor: "#BEAD8E",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        })}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: "Hotel Miranda" }}
         />
-        <Stack.Screen name="Info" component={InfoScreen} />
+        <Stack.Screen
+          name="CheckIn"
+          component={CheckInScreen}
+          options={{ title: "Check In" }}
+        />
+        <Stack.Screen
+          name="Info"
+          component={InfoScreen}
+          options={({ navigation }) => ({
+            title: "Info",
+
+            headerRight: () => (
+              <Pressable onPress={() => navigation.navigate("Home")}>
+                <Text style={styles.linkText}>Home</Text>
+              </Pressable>
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
