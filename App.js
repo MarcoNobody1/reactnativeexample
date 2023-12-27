16551855814e66d7d9d486703b6635ec799f9ab0
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-27 11:26:28 +0100

    app.js created, testing it

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Buenas tardes!</Text>
+      <Text style={styles.second}>Puede traerme un vaso de agua?</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    backgroundColor: "red",
+    padding: 20,
+  },
+  second: {
+    margin: "50px",
   },
 });
