1e31d0b86a3348bb99fb58248d043b4fe56e209a
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-27 14:40:35 +0100

    styles folder created

---
+++ styles/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    backgroundColor: "grey",
+    padding: 20,
+  },
+  second: {
+    margin: 20,
+  },
+  bar: {
+    backgroundColor: "blue",
+  },
+});
