f261809c9820e6f9b8f44df93d7cc6e3e777b154
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-27 19:59:46 +0100

    ended styling in home screen

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,14 +1,41 @@
-import { Button, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { HomeProps } from "../interfaces/NavigationInterfaces";
 import { styles } from "../styles/styles";
+import React, { FC, useState } from "react";
 
-export const HomeScreen: React.FC<HomeProps> = ({ navigation }) => {
+export const HomeScreen: FC<HomeProps> = ({ navigation }) => {
+  const [bookingNumb, setBookingNumb] = useState<string>("");
+
   return (
-    <View style={styles.container}>
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigation.navigate("Info", { name: "Jane" })}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.homeform}>
+        <Text style={styles.homeheading}>Booking Reference Number:</Text>
+        <TextInput
+          style={styles.homeinput}
+          placeholder="e.g. : 0000"
+          keyboardType="number-pad"
+          maxLength={4}
+          placeholderTextColor={"#686868"}
+          returnKeyLabel="Booking"
+          onChangeText={setBookingNumb}
+        />
+        <Pressable
+          style={bookingNumb.length !== 4 ? styles.homebtndisabled : styles.homebtn}
+          disabled={bookingNumb.length !== 4 ? true : false}
+          onPress={() =>
+            navigation.navigate("CheckIn", { booking: bookingNumb })
+          }
+        >
+          <Text style={styles.homebtntext}>Check In</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
