0d2115708d39764154358f044415aeabb438d3a9
MarcoNobody1 <marcocamaradiaz@gmail.com>
2024-01-03 19:29:30 +0100

    conditional rendering implementation using usestate

--- App.tsx
+++ App.tsx
@@ -4,20 +4,31 @@ import { HomeScreen } from "./app/HomeScreen";
 import { CheckInScreen } from "./app/CheckInScreen";
 import { RootStackParamList } from "./interfaces/NavigationInterfaces";
 import { Logo } from "./components/logo";
-import { Pressable, Text } from "react-native";
+import { Alert, Pressable, Text } from "react-native";
 import { styles } from "./styles/styles";
 import { InfoScreen } from "./app/InfoScreen";
+import { LoginScreen } from "./app/LoginScreen";
+import { useState } from "react";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [logged, setLogged] = useState(false);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
+        initialRouteName="Login"
         screenOptions={({ navigation }) => ({
-          headerTitle: (props) => <Logo navigation={navigation} />,
+          headerTitle: (props) => <Logo navigation={navigation}/>,
           headerRight: () => (
-            <Pressable onPress={() => navigation.navigate("Info")}>
+            <Pressable
+              onPress={() =>
+                logged
+                  ? navigation.navigate("Info")
+                  : Alert.alert("You're not logged")
+              }
+            >
               <Text style={styles.linkText}>Info</Text>
             </Pressable>
           ),
@@ -31,28 +42,38 @@ export default function App() {
         })}
       >
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Hotel Miranda" }}
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Login" }}
         />
-        <Stack.Screen
-          name="CheckIn"
-          component={CheckInScreen}
-          options={{ title: "Check In" }}
-        />
-        <Stack.Screen
-          name="Info"
-          component={InfoScreen}
-          options={({ navigation }) => ({
-            title: "Info",
 
-            headerRight: () => (
-              <Pressable onPress={() => navigation.navigate("Home")}>
-                <Text style={styles.linkText}>Home</Text>
-              </Pressable>
-            ),
-          })}
-        />
+        {logged ? (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ title: "Hotel Miranda" }}
+            />
+            <Stack.Screen
+              name="CheckIn"
+              component={CheckInScreen}
+              options={{ title: "Check In" }}
+            />
+            <Stack.Screen
+              name="Info"
+              component={InfoScreen}
+              options={({ navigation }) => ({
+                title: "Info",
+
+                headerRight: () => (
+                  <Pressable onPress={() => navigation.navigate("Home")}>
+                    <Text style={styles.linkText}>Home</Text>
+                  </Pressable>
+                ),
+              })}
+            />
+          </>
+        ) : null}
       </Stack.Navigator>
     </NavigationContainer>
   );
