52a036cba998ec6a17af71037655434a19b3bf7b
MarcoNobody1 <marcocamaradiaz@gmail.com>
2024-01-04 13:00:56 +0100

    loginthunk finished

--- features/Login/loginThunks.ts
+++ features/Login/loginThunks.ts
@@ -1,14 +1,37 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { LoginDataInterface } from "../../interfaces/LoginDataInterface";
-import { LoginIntialState } from "../../interfaces/SlicesInterfaces";
+import { FETCH_URL } from "@env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const userLogin = createAsyncThunk<LoginIntialState>(
+export const userLogin = createAsyncThunk(
   "login/userLogin",
   async (log: LoginDataInterface) => {
     try {
-        const response 
+      const response = await fetch(`${FETCH_URL}/login`, {
+        mode: "cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: log.username,
+          password: log.password,
+        }),
+      });
+      if (!response.ok) throw new Error(`Status: ${response.status}`);
+      const data = await response.json();
+      await AsyncStorage.setItem("token", data.token);
+      await AsyncStorage.setItem(
+        "userData",
+        JSON.stringify({
+          username: data.payload.username,
+          email: data.payload.email,
+          avatar: data.payload.avatar,
+        })
+      );
+      return data;
     } catch (error) {
-        throw error;
+      throw new Error(`${error}`);
     }
   }
 );
