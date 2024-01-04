dccc677109a123ea96b07748eba34aaba78b4985
MarcoNobody1 <marcocamaradiaz@gmail.com>
2024-01-04 13:03:15 +0100

    moved fetch to loginthunk, working on login auth

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -1,91 +1,93 @@
-import { Alert, Pressable, SafeAreaView, Text, TextInput, View } from "react-native";
+import {
+  ActivityIndicator,
+  Pressable,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { LoginProps } from "../interfaces/NavigationInterfaces";
 import { styles } from "../styles/styles";
-import React, { FC, useState } from "react";
-import { LoginDataInterface } from "../interfaces/LoginDataInterface";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { FC, useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { userLogin } from "../features/Login/loginThunks";
+import { loginStatus, resetState } from "../features/Login/loginSlice";
 
 export const LoginScreen: FC<LoginProps> = ({ navigation }) => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const dispatch = useAppDispatch();
+  const status = useAppSelector(loginStatus);
 
-  const LoginCheck = async (loginData: LoginDataInterface) => {
-    let token: string;
-
-    try {
-      const response = await fetch(
-        `https://7zclei7sla.execute-api.eu-west-1.amazonaws.com/login`,
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(loginData),
-        }
-      );
-      if (!response.ok) throw new Error(`Status: ${response.status}`);
-      const data = await response.json();
-      await AsyncStorage.setItem("token", data.token);
+  useEffect(() => {
+    if (status === "fulfilled") {
       navigation.navigate("Home");
-    } catch (error) {
-        Alert.alert("Username or Password incorrect!", "Please, try again...")
+
     }
-  };
+  }, [status]);
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.homeform}>
-        <Text nativeID="inputUsername" style={styles.homeheading}>
-          Username:
-        </Text>
-        <TextInput
-          style={styles.homeinput}
-          placeholder="e.g. : HotelEnjoyer_69"
-          placeholderTextColor={"#686868"}
-          returnKeyLabel="username"
-          onChangeText={setUsername}
-          value={username}
-          accessibilityLabel="inputUsername"
-          accessibilityLabelledBy="inputUsername"
-          textContentType="username"
-          autoComplete="username"
-          autoFocus
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-        <Text nativeID="inputPassword" style={styles.homeheading}>
-          Password:
-        </Text>
-        <TextInput
-          style={styles.homeinput}
-          placeholder="e.g. : 123456"
-          secureTextEntry={true}
-          placeholderTextColor={"#686868"}
-          returnKeyLabel="Booking"
-          onChangeText={setPassword}
-          value={password}
-          accessibilityLabel="inputPassword"
-          accessibilityLabelledBy="inputPassword"
-          textContentType="password"
-          autoCapitalize="none"
-        />
-        <Pressable
-          style={password.length < 5 ? styles.homebtndisabled : styles.homebtn}
-          disabled={password.length < 5 ? true : false}
-          onPress={() => {
-            const loginData = {
-              username: username,
-              password: password,
-            };
+        {status === "pending" ? (
+          <ActivityIndicator size={"large"} />
+        ) : (
+          <>
+            <Text nativeID="inputUsername" style={styles.homeheading}>
+              Username:
+            </Text>
+            <TextInput
+              style={styles.homeinput}
+              placeholder="e.g. : HotelEnjoyer_69"
+              placeholderTextColor={"#686868"}
+              returnKeyLabel="username"
+              onChangeText={setUsername}
+              value={username}
+              accessibilityLabel="inputUsername"
+              accessibilityLabelledBy="inputUsername"
+              textContentType="username"
+              autoComplete="username"
+              autoFocus
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <Text nativeID="inputPassword" style={styles.homeheading}>
+              Password:
+            </Text>
+            <TextInput
+              style={styles.homeinput}
+              placeholder="e.g. : 123456"
+              secureTextEntry={true}
+              placeholderTextColor={"#686868"}
+              returnKeyLabel="Booking"
+              onChangeText={setPassword}
+              value={password}
+              accessibilityLabel="inputPassword"
+              accessibilityLabelledBy="inputPassword"
+              textContentType="password"
+              autoCapitalize="none"
+            />
+            <Pressable
+              style={
+                password.length < 5 ? styles.homebtndisabled : styles.homebtn
+              }
+              disabled={password.length < 5 ? true : false}
+              onPress={() => {
+                const loginData = {
+                  username: username,
+                  password: password,
+                };
 
-            LoginCheck(loginData);
-            setUsername("");
-            setPassword("");
-          }}
-        >
-          <Text style={styles.homebtntext}>Login</Text>
-        </Pressable>
+                dispatch(userLogin(loginData));
+
+                setUsername("");
+                setPassword("");
+              }}
+            >
+              <Text style={styles.homebtntext}>Login</Text>
+            </Pressable>
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
