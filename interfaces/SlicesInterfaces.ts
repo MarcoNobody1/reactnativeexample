10e212376d6dce29cdd32a9a4a36e82823600b18
MarcoNobody1 <marcocamaradiaz@gmail.com>
2024-01-04 13:01:50 +0100

    login initial state interface fixed

--- interfaces/SlicesInterfaces.ts
+++ interfaces/SlicesInterfaces.ts
@@ -1,7 +1,11 @@
 import { LoginDataInterface } from "./LoginDataInterface";
 
 export type LoginIntialState = {
-    error: string | undefined;
-    status: "idle" | "fulfilled" | "pending" | "rejected";
-    data: LoginDataInterface
-}
+  error: string | undefined;
+  status: "idle" | "fulfilled" | "pending" | "rejected";
+  loginInfo: {
+    username: string;
+    email: string;
+    photo: string;
+  };
+};
